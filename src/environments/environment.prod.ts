a6d32b8b1f5d9125003384e03f1d61f4096d7369
eduardogb <eduardo.garciab@hotmail.com>
2021-01-27 16:07:35 -0600

    [ADD] Environments without keys

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: true
+};
+
+export const APY_KEY = '';
+
+export const URL_API = '';
+
+export const DEFAULT_IMAGE = 'https://firebasestorage.googleapis.com/v0/b/portfolio-ed.appspot.com/o/images%2FAppleMusic%20.jpg?alt=media&token=7829ed1a-f6f4-423c-84e4-738f8299b6f4';
+
+export const GOOGLE_API = '';
+
+export const firebaseConfig = {
+};
+
+export const COLLECTION_NAME = '';
+
+export const ID = '';
+export const LOCALE = '';
+
+export const RAPPI_KEY = '';
+
+export const HOST = '';
---
+++ src/environments/environment.ts
@@ -0,0 +1,36 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false
+};
+
+export const APY_KEY = '';
+
+export const URL_API = '';
+
+export const DEFAULT_IMAGE = 'https://firebasestorage.googleapis.com/v0/b/portfolio-ed.appspot.com/o/images%2FAppleMusic%20.jpg?alt=media&token=7829ed1a-f6f4-423c-84e4-738f8299b6f4';
+
+export const GOOGLE_API = '';
+
+export const firebaseConfig = {
+
+};
+
+export const COLLECTION_NAME = '';
+
+export const ID = '';
+export const LOCALE = '';
+
+export const RAPPI_KEY = '';
+
+export const HOST = '';
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
